8a1d00f39fb656c9a2e656aff5f90f1d8c18c8fd
Raphael Freitas <raphael@hawks.com.br>
2021-01-13 13:43:57 -0300

    creating item list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,13 +23,40 @@ class _HomePageState extends State<HomePage> {
     var size = MediaQuery.of(context).size;
     return ListView(children: [
       Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        SizedBox(
-          height: 15,
-        ),
         headerComponent(),
         searchBar(size),
         slider(),
         fastIcons(size),
+        Container(
+            width: size.width,
+            child: Padding(
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          width: size.width,
+                          height: 160,
+                          child: Image(
+                              image: NetworkImage(firstMenu[0]['img']),
+                              fit: BoxFit.cover),
+                        ),
+                        Positioned(
+                          bottom: 15,
+                          right: 15,
+                          child: SvgPicture.asset(
+                              firstMenu[0]['is_liked']
+                                  ? 'assets/images/loved_icon.svg'
+                                  : 'assets/images/love_icon.svg',
+                              width: 20,
+                              color: white),
+                        ),
+                      ],
+                    )
+                  ],
+                )))
       ])
     ]);
   }
@@ -37,6 +64,7 @@ class _HomePageState extends State<HomePage> {
   Widget headerComponent() {
     return Column(
       children: [
+        SizedBox(height: 15),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(menu.length, (index) {
@@ -181,41 +209,46 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget fastIcons(size) {
-    return Container(
-      width: size.width,
-      decoration: BoxDecoration(color: textFieldColor),
-      child: Padding(
-        padding: EdgeInsets.only(bottom: 10, top: 10),
-        child: Container(
-          decoration: BoxDecoration(color: white),
+    return Column(
+      children: [
+        Container(
+          width: size.width,
+          decoration: BoxDecoration(color: textFieldColor),
           child: Padding(
-            padding: EdgeInsets.only(top: 15, bottom: 15),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                margin: EdgeInsets.only(left: 30),
-                child: Row(
-                  children: List.generate(categories.length, (index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 25),
-                      child: Column(children: [
-                        SvgPicture.asset(
-                          categories[index]['img'],
-                          width: 40,
-                        ),
-                        SizedBox(height: 8),
-                        Text(categories[index]['name'],
-                            style: TextStyle(
-                                fontSize: 13, fontWeight: FontWeight.w500))
-                      ]),
-                    );
-                  }),
+            padding: EdgeInsets.only(bottom: 10, top: 10),
+            child: Container(
+              decoration: BoxDecoration(color: white),
+              child: Padding(
+                padding: EdgeInsets.only(top: 15, bottom: 15),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    child: Row(
+                      children: List.generate(categories.length, (index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 25),
+                          child: Column(children: [
+                            SvgPicture.asset(
+                              categories[index]['img'],
+                              width: 40,
+                            ),
+                            SizedBox(height: 8),
+                            Text(categories[index]['name'],
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.w500))
+                          ]),
+                        );
+                      }),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
         ),
-      ),
+        SizedBox(height: 15)
+      ],
     );
   }
 }
